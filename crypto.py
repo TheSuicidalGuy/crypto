fcf85d1218f4e87803260424a76fc9d495ae541a
Nour BENALI <nourrbenali0@gmail.com>
2025-03-10 16:59:00 +0100

    updaaaate

--- crypto.py
+++ crypto.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 
-Messageacrypter= input("Veuillez ecrire une phrase: ")
+root = tk.Tk()
+root.title("Chiffrement césar")
+root.geometry("300x300")
+
+button1 = tk.Button(root, text="Veuillez ecrire une phrase:", command=lambda: print("") ) 
+button1.grid(row=1, column=1, pady=10)
+
+
+Messageacrypter= input("Veuillez ecrire une phrase : ")
 cle=int(input("Veuiller entrer la clé: "))
 
 acrypter=Messageacrypter.upper()
@@ -15,3 +23,4 @@ for i in range(lg):
         MessageCrypte+=chr(asc+26*((asc<65)-(asc>90)))
 
 print(MessageCrypte)
+#https://fraoustin.fr/old/20111222_1.html
