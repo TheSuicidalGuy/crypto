3bd6ac9ae67aa2cc992ffedae3cdd274ddf27ba6
Nour BENALI <nourrbenali0@gmail.com>
2025-05-01 20:49:45 +0200

    j a essayer d'expliquer les lignes la

--- cryptanalyse de vigenère.py
+++ cryptanalyse de vigenère.py
@@ -1,12 +1,13 @@
 # test 1 #
 
-message = "SEMAINE"
-cle = "CLE"
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-message_code = ""
+message =  input("Veuillez ecrire un message : ")  # Message à chiffrer
+cle = input("Veuillez entrer la clé : ")  # Clé de chiffrement
+message = message.upper()  # Convertir le message en majuscules
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # Alphabet utilisé pour le chiffrement
+message_code = "" # Initialiser le message chiffré
 
-for i in range(len(message)):
-    lettre_message = message[i]
+for i in range(len(message)): # Parcourir chaque lettre du message
+    lettre_message = message[i] # Lettre du message
     lettre_cle = cle[i % len(cle)]
     indice = (alphabet.index(lettre_message) + alphabet.index(lettre_cle)) % len(alphabet)
     message_code += alphabet[indice]
